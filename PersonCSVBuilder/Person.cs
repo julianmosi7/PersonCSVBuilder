5f65fdd6f951ac19f88168ac7e8fa0907ec1a64f
Julian <moshammer.ju@gmail.com>
2021-03-11 18:55:38 +0100

    init

---
+++ PersonCSVBuilder/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonCSVBuilder
+{
+    public class Person
+    {
+        private readonly string firstname;
+        private readonly string lastname;
+        private readonly int age;
+        private readonly string phone;
+        private readonly string address;
+
+        public string Firstname => firstname;
+        public string Lastname => lastname;
+        public int Age => age;
+        public string Phone => phone;
+        public string Address => address;
+        public override string ToString() => $"{firstname} {lastname} {age} {phone} {address}";
+
+        private Person(Builder builder)
+        {
+            this.firstname = builder.firstname;
+            this.lastname = builder.lastname;
+            this.age = builder.age;
+            this.phone = builder.phone;
+            this.address = builder.address;
+        }
+
+        public class Builder
+        {
+            internal readonly string firstname;
+            internal readonly string lastname;
+            internal int age;
+            internal string phone;
+            internal string address;
+
+            public Builder(string firstname, string lastname)
+            {
+                this.firstname = firstname;
+                this.lastname = lastname;
+            }
+
+            public Builder Age(int age)
+            {
+                this.age = age;
+                return this;
+            }
+
+            public Builder Phone(string phone)
+            {
+                this.phone = phone;
+                return this;
+            }
+
+            public Builder Address(string address)
+            {
+                this.address = address;
+                return this;
+            }
+
+            public Person Build()
+            {
+                var person = new Person(this);
+                if (person.age > 115) throw new InvalidOperationException($"invalid age {person.age}");
+                if (person.firstname.Length < 3) throw new InvalidOperationException($"invalid firstname {person.firstname}");
+                return person;
+            }
+        }
+    }
+}
---
+++ PersonCSVBuilder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PersonCSVBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var builder = new Person.Builder("Julian", "Moshammer");
+
+            //var person = builder.Build();
+            //var person2 = builder.Age(18).Build();
+            //Console.WriteLine(person);
+            //Console.WriteLine(person2);
+
+            //Console.ReadKey();
+            List<Person> persons = new List<Person>();
+            var lines = File.ReadAllLines("persons_with_address.csv").Skip(1).Select(x => x.Split(";"));
+            foreach (var line in lines)
+            {
+                try
+                {
+                    var person = new Person.Builder(line[0], line[1]).Age(Int32.Parse(line[2])).Address(line[2]).Phone(line[3]).Build();
+                    persons.Add(person);
+                }
+                catch(InvalidOperationException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+                
+            }
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
